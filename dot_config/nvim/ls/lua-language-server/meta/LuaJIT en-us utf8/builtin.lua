d42f5c7e99fd9ae538728c67a2164d7b10347f84
domogami <domogami@gmail.com>
2022-04-08 15:47:17 -0700

    Added .fig

---
+++ dot_config/nvim/ls/lua-language-server/meta/LuaJIT en-us utf8/builtin.lua
@@ -0,0 +1,14 @@
+---@meta
+
+---@class unknown
+---@class any
+---@class nil
+---@class boolean
+---@class number
+---@alias integer number
+---@class thread
+---@class table
+---@class string
+---@class userdata
+---@class lightuserdata
+---@class function
---
+++ dot_config/nvim/lua/colorscheme/palenight.lua
@@ -0,0 +1,4 @@
+Vapour.utils.plugins.packadd('drewtempelmeyer/palenight.vim')
+vim.g.termguicolors = true
+vim.cmd('colorscheme palenight')
+Vapour.settings.lualine_colorscheme = 'palenight'
